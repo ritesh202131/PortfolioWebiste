a18d3b56c0267c0df3570602025b7204b0abd6a5
ritesh202131 <ritesh3822mohapatra@gmail.com>
2025-09-03 08:53:07 +0530

    first commit

---
+++ starter-template/src/sections/Header.tsx
@@ -0,0 +1,14 @@
+import { clearScreenDown } from "readline";
+
+export const Header = () => {
+  return <div>Header Section
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Projects</a>
+      <a href="#">About Me</a>
+      <a href="#">Contact</a>
+
+    </nav>
+
+  </div>;
+};
